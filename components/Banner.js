3832b2957f1fe7e3b469e584ea095b4354c0feac
rbruyninckx <ryan@rybx.com>
2023-02-03 18:19:06 -0800

    upd banner

--- components/Banner.js
+++ components/Banner.js
@@ -7,15 +7,15 @@ export default function Banner({title, src, linkText, closeIcon}) {
         banner.classList.add("hidden");
     }
     return(
-        <div className="bg-logo w-full text-center lg:text-xl text-white px-10 py-4 relative banner">
+        <div className="bg-logo w-full text-center text-sm lg:text-xl text-white px-10 py-4 relative banner">
             {title}<br className="lg:hidden"/>
             <Link href={src} className="ml-2 underline underline-offset-2 text-white hover:text-logo-light hover:underline-logo-light">
                 {linkText}
             </Link>
             { closeIcon == "true" ? 
-            <div className="absolute -top-4 right-8 lg:top-0 lg:right-10 h-full w-5 lg:w-8 flex items-center" >
-                <div className="w-5 h-5 lg:w-8 lg:h-8" onClick={closeBanner}>
-                    <i className="fa-light fa-xmark h-5 w-5 lg:h-8 lg:w-8 hover:cursor-pointer  opacity-60 hover:opacity-100" title="Close Banner" ></i>
+            <div className="absolute top-0 right-10 h-full w-8 flex items-center" >
+                <div className="w-8 h-8" onClick={closeBanner}>
+                    <i className="fa-light fa-xmark h-8 w-8 hover:cursor-pointer  opacity-60 hover:opacity-100" title="Close Banner" ></i>
                 </div>
             </div>
             : null
